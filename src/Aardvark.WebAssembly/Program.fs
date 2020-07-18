bd8f6c146294309280780537bea14db12eeb600b
Georg Haaser <krauthaufen@awx.at>
2020-07-18 14:09:10 +0200

    adaptive working

--- src/Aardvark.WebAssembly/Program.fs
+++ src/Aardvark.WebAssembly/Program.fs
@@ -4,7 +4,6 @@ module Bla
 open System
 open WebAssembly
 open WebAssembly.Core
-
 // C:\Users\Schorsch\Downloads\mono-wasm-28315c66b74>
 //    packager.exe --appdir=bla --search-path=C:\Users\Schorsch\.nuget\packages\fsharp.core\4.7.0\lib\netstandard2.0 "C:\Users\Schorsch\Development\wasm\Wasm\bin\Debug\netstandard2.1\Wasm.dll" C:\Users\Schorsch\.nuget\packages\fsharp.core\4.7.0\lib\netstandard2.0\FSharp.Core.dll
 
@@ -50,6 +49,10 @@ let inline (?<-) (o : JsObj) (name : string) (value : 'a) =
     | :? JsObj as v -> o.[name] <- v
     | _ -> o.[name] <- value
 
+type ShaderType =
+    | Vertex = 0x8B31
+    | Fragment = 0x8B30
+
 
 [<Flags>]
 type ClearBuffers =
@@ -66,6 +69,37 @@ type BufferTarget =
     | Uniform = 0x8A11
     | PixelPack = 0x88EB
     | PixelUnpack = 0x88EC
+
+type BufferUsage =
+    | StaticDraw = 0x88E4
+    | StreamDraw = 0x88E0
+    | DynamicDraw = 0x88E8
+    
+    | StaticRead = 0x88E5
+    | StreamRead = 0x88E1
+    | DynamicRead = 0x88E9
+    
+    | StaticCopy = 0x88E6
+    | StreamCopy = 0x88E2
+    | DynamicCopy = 0x88EA
+
+type VertexAttribType =
+    | Byte = 0x1400
+    | UnsignedByte = 0x1401
+    | Short = 0x1402
+    | UnsignedShort = 0x1403
+    | Int = 0x1404
+    | UnsignedInt = 0x1405
+    | Float = 0x1406
+    
+type PrimitiveTopology =
+    | Points = 0x0000
+    | Lines = 0x0001
+    | LineLoop = 0x0002
+    | LineStrip = 0x0003
+    | Triangles = 0x0004
+    | TriangleStrip = 0x0005
+    | TriangleFan = 0x0006
 
 type FramebufferTarget =
     | Framebuffer = 0x8D40
@@ -84,6 +118,24 @@ type BlendEquation =
     | ReverseSubtract   = 0x800B
     //| Min
     //| Max
+
+type BlendFactor =
+    | Zero = 0
+    | One = 1
+    | SrcColor = 0x0300
+    | OneMinusSrcColor = 0x0301
+    | DstColor = 0x0306
+    | OneMinusDstColor = 0x0307
+    | SrcAlpha = 0x0302
+    | OneMinusSrcAlpha = 0x0303
+    | DstAlpha = 0x0304
+    | OneMinusDstAlpha = 0x0305
+    | ConstantColor = 0x8001
+    | OneMinusConstantColor = 0x8002
+    | ConstantAlpha = 0x8003
+    | OneMinusConstantAlpha = 0x8004
+    | SrcAlphaSaturate = 0x0308
+
 
 [<AllowNullLiteral>]
 type WebGLProgram(ref : JSObject) =
@@ -155,12 +207,90 @@ type WebGLRenderingContext(ref : JSObject) =
     /// The WebGLRenderingContext.blendEquation() method of the WebGL API is used to set both the RGB blend equation and alpha blend equation to a single equation.
     /// The blend equation determines how a new pixel is combined with a pixel already in the WebGLFramebuffer.
     member x.BlendEquation(mode : BlendEquation) =
-        ref.Invoke("blendEquation", int mode)
+        ref.Invoke("blendEquation", int mode) |> ignore
 
     /// The WebGLRenderingContext.blendEquationSeparate() method of the WebGL API is used to set the RGB blend equation and alpha blend equation separately.
     /// The blend equation determines how a new pixel is combined with a pixel already in the WebGLFramebuffer.
     member x.BlendEquation(rgb : BlendEquation, alpha : BlendEquation) =
-        ref.Invoke("blendEquationSeparate", int rgb, int alpha)
+        ref.Invoke("blendEquationSeparate", int rgb, int alpha) |> ignore
+
+    /// The WebGLRenderingContext.blendFunc() method of the WebGL API defines which function is used for blending pixel arithmetic.
+    member x.BlendFunc(src : BlendFactor, dst : BlendFactor) =
+        ref.Invoke("blendFunc", int src, int dst) |> ignore
+        
+    /// The WebGLRenderingContext.blendFuncSeparate() method of the WebGL API defines which function is used for blending pixel arithmetic for RGB and alpha components separately.
+    member x.BlendFunc(srcRGB : BlendFactor, dstRGB : BlendFactor, srcAlpha : BlendFactor, dstAlpha : BlendFactor) =
+        ref.Invoke("blendFuncSeparate", int srcRGB, int dstRGB, int srcAlpha, int dstAlpha) |> ignore
+        
+
+    /// The WebGLRenderingContext.bufferData() method of the WebGL API initializes and creates the buffer object's data store.
+    member x.BufferData(target : BufferTarget, size : int, usage : BufferUsage) =
+        ref.Invoke("bufferData", int target, size, int usage) |> ignore
+
+    /// The WebGLRenderingContext.bufferData() method of the WebGL API initializes and creates the buffer object's data store.
+    member x.BufferData(target : BufferTarget, data : ArrayBuffer, usage : BufferUsage) =
+        ref.Invoke("bufferData", int target, data, int usage) |> ignore
+        
+    /// The WebGLRenderingContext.bufferData() method of the WebGL API initializes and creates the buffer object's data store.
+    member x.BufferData(target : BufferTarget, data : SharedArrayBuffer, usage : BufferUsage) =
+        ref.Invoke("bufferData", int target, data, int usage) |> ignore
+
+    /// The WebGLRenderingContext.bufferData() method of the WebGL API initializes and creates the buffer object's data store.
+    member x.BufferData(target : BufferTarget, data : TypedArray<_,_>, usage : BufferUsage) =
+        ref.Invoke("bufferData", int target, data, int usage) |> ignore
+        
+    /// The WebGLRenderingContext.bufferSubData() method of the WebGL API updates a subset of a buffer object's data store.
+    member x.BufferSubData(target : BufferTarget, offset : int, data : ArrayBuffer) =
+        ref.Invoke("bufferData", int target, offset, data) |> ignore
+        
+    /// The WebGLRenderingContext.bufferSubData() method of the WebGL API updates a subset of a buffer object's data store.
+    member x.BufferSubData(target : BufferTarget, offset : int, data : SharedArrayBuffer) =
+        ref.Invoke("bufferData", int target, offset, data) |> ignore
+
+    /// The WebGLRenderingContext.bufferSubData() method of the WebGL API updates a subset of a buffer object's data store.
+    member x.BufferSubData(target : BufferTarget, offset : int, data : TypedArray<_,_>) =
+        ref.Invoke("bufferSubData", int target, offset, data) |> ignore
+
+    /// The WebGLRenderingContext.createBuffer() method of the WebGL API creates and initializes a WebGLBuffer storing data such as vertices or colors.
+    member x.CreateBuffer() =
+        ref.Invoke("createBuffer") |> unbox<JSObject> |> WebGLBuffer
+
+    member x.CreateShader(typ : ShaderType) =
+        ref.Invoke("createShader", int typ) |> unbox<JSObject> |> WebGLShader
+        
+    member x.ShaderSource(shader : WebGLShader, source : string) =
+        ref.Invoke("shaderSource", shader.Reference, source) |> ignore
+        
+    member x.CompileShader(shader : WebGLShader) =
+        ref.Invoke("compileShader", shader.Reference) |> ignore
+        
+    member x.CreateProgram() =
+        ref.Invoke("createProgram") |> unbox<JSObject> |> WebGLProgram
+        
+    member x.LinkProgram(program : WebGLProgram) =
+        ref.Invoke("linkProgram", program.Reference) |> ignore
+        
+    member x.GetShaderInfoLog(shader : WebGLShader) =
+        ref.Invoke("getShaderInfoLog", shader.Reference) |> unbox<string>
+
+    member x.GetProgramInfoLog(program : WebGLProgram) =
+        ref.Invoke("getProgramInfoLog", program.Reference) |> unbox<string>
+
+    member x.UseProgram(program : WebGLProgram) =
+        if isNull program then ref.Invoke("useProgram", [| null |]) |> ignore
+        else ref.Invoke("useProgram", program.Reference) |> ignore
+    
+    member x.EnableVertexAttribArray(index : int) =
+        ref.Invoke("enableVertexAttribArray", index) |> ignore
+
+    member x.DisableVertexAttribArray(index : int) =
+        ref.Invoke("disableVertexAttribArray", index) |> ignore
+
+    member x.VertexAttribPointer(index : int, size : int, typ : VertexAttribType, normalized : bool, stride : int, offset : int) =
+        ref.Invoke("vertexAttribPointer", index, size, int typ, normalized, stride, offset) |> ignore
+
+    member x.DrawArrays(mode : PrimitiveTopology, first : int, count : int) =
+        ref.Invoke("drawArrays", int mode, first, count) |> ignore
 
     member x.ClearColor(r : float, g : float, b : float, a : float) =
         ref.Invoke("clearColor", r, g, b, a) |> ignore
@@ -332,6 +462,15 @@ type HTMLDocument(r : JSObject) =
 type Console(r : JSObject) =
     inherit JsObj(r)
 
+    member x.Begin(name : string) =
+        r.Invoke("group", name) |> ignore
+        
+    member x.BeginCollapsed(name : string) =
+        r.Invoke("groupCollapsed", name) |> ignore
+
+    member x.End() =
+        r.Invoke("groupEnd") |> ignore
+
     member x.Log([<ParamArray>] values : obj[]) =
         r.Invoke("log", values) |> ignore
     
@@ -347,11 +486,55 @@ module RuntimValues =
 
 
 
+//open FSharp.Data.Adaptive
+//open System.Reflection.Emit
+//open System.Reflection
+
+//let testDynamicMethod() =
+//    Console.Begin "DynamicMethod"
+//    let m = DynamicMethod("bla", typeof<int>, [| typeof<int> |] )
+//    let il = m.GetILGenerator()
+//    il.Emit(OpCodes.Ldarg_0)
+//    il.Emit(OpCodes.Ldc_I4, 5)
+//    il.Emit(OpCodes.Add)
+//    il.Emit(OpCodes.Ret)
+
+//    let f = m.CreateDelegate(typeof<Func<int, int>>) |> unbox<Func<int, int>>
+//    Console.Log(15, "=?=", f.Invoke(10))
+//    Console.End()
+
+open FSharp.Data.Adaptive
+
+let testAdaptive() =
+    Console.Begin "Adaptive"
+    let a = cval 10
+    let b = a |> AVal.map ((+) 5) 
+    printfn "%A =?= 15" (AVal.force b)
+    transact (fun () -> a.Value <- 5)
+    printfn "%A =?= 10" (AVal.force b)
+
+    Console.End()
+
+let testArrayBuffer() =
+    Console.Begin "ArrayBuffer"
+    let data = Array.init 1024 byte
+    use a = Uint8Array.op_Implicit(Span data)
+
+    let print = Runtime.CompileFunction("return function(a) { console.log('js[0]', a[0], '=?=', 100); };")
+    let f = Runtime.CompileFunction("return function(a) { a[0] = 100; };")
+    f.Call(null, a) |> ignore
+
+    Console.Log("f#[0]", a.[0], "=?=", 100)
+    print.Call(null, a) |> ignore
+    Console.End()
+
 
 [<EntryPoint>]
 let main _argv =
-    let data = Array.init 1024 byte
-    use a = Uint8Array.op_Implicit(Span data)
+    testAdaptive()
+    //testDynamicMethod()
+    testArrayBuffer()
+
 
     let c = Document.CreateCanvasElement()
     c.Id <- "bla"
@@ -366,15 +549,89 @@ let main _argv =
     gl.ClearColor(1.0, 0.0, 1.0, 1.0)
     gl.Clear ClearBuffers.Color
 
-    Console.Warn "done"
 
-    let f = Runtime.CompileFunction("return function(a) { console.log('hans', a); };")
-    f.Call(null, a) |> ignore
+    let pos = Float32Array.op_Implicit (Span [| 0.0f; 0.0f; 0.0f; 1.0f; 0.0f; 0.0f; 1.0f; 1.0f; 0.0f|])
+    let col = Uint8Array.op_Implicit (Span [|255uy; 0uy; 0uy; 255uy; 0uy; 255uy; 0uy; 255uy; 0uy; 0uy; 255uy; 255uy|])
 
-    let f = Runtime.CompileFunction("return function(a) { a[0] = 100; };")
-    f.Call(null, a) |> ignore
+    let pb = gl.CreateBuffer()
+    gl.BindBuffer(BufferTarget.Array, pb)
+    gl.BufferData(BufferTarget.Array, pos, BufferUsage.StaticDraw)
+    gl.BindBuffer(BufferTarget.Array, null)
+    
+    let cb = gl.CreateBuffer()
+    gl.BindBuffer(BufferTarget.Array, cb)
+    gl.BufferData(BufferTarget.Array, col, BufferUsage.StaticDraw)
+    gl.BindBuffer(BufferTarget.Array, null)
 
-    Console.Log("a[0]: ", a.[0])
+
+    let vsc =
+        """#version 300 es
+        layout(location = 0) in vec4 v_position;
+        layout(location = 1) in vec4 v_color;
+
+        out vec4 f_color;
+
+        void main() {
+            gl_Position = v_position;
+            f_color = v_color;
+        }
+
+
+        """
+
+    let psc = 
+        """#version 300 es
+        precision highp float;
+        in vec4 f_color;
+        out vec4 c;
+
+        void main() {
+            c = f_color;
+        }
+
+        """
+
+    let vs = gl.CreateShader(ShaderType.Vertex)
+    gl.ShaderSource(vs, vsc)
+    gl.CompileShader(vs)
+    let log = gl.GetShaderInfoLog vs
+    if not (String.IsNullOrWhiteSpace log) then
+        Console.Warn(log)
+
+
+    let fs = gl.CreateShader(ShaderType.Fragment)
+    gl.ShaderSource(fs, psc)
+    gl.CompileShader(fs)
+    let log = gl.GetShaderInfoLog fs
+    if not (String.IsNullOrWhiteSpace log) then
+        Console.Warn(log)
+
+    let p = gl.CreateProgram()
+    gl.AttachShader(p, vs)
+    gl.AttachShader(p, fs)
+    gl.LinkProgram(p)
+
+    let log = gl.GetProgramInfoLog(p)
+    if not (String.IsNullOrWhiteSpace log) then
+        Console.Warn(log)
+
+
+    gl.UseProgram(p)
+
+    gl.BindBuffer(BufferTarget.Array, pb)
+    gl.EnableVertexAttribArray 0
+    gl.VertexAttribPointer(0, 3, VertexAttribType.Float, false, 12, 0)
+
+    
+    gl.BindBuffer(BufferTarget.Array, cb)
+    gl.EnableVertexAttribArray 1
+    gl.VertexAttribPointer(1, 4, VertexAttribType.UnsignedByte, true, 4, 0)
+    
+
+    gl.DrawArrays(PrimitiveTopology.Triangles, 0, 3)
+
+    gl.UseProgram(null)
+
 
 
 
--- src/Aardvark.WebAssembly/package.bat
+++ src/Aardvark.WebAssembly/package.bat
@@ -1,8 +1,44 @@
 @echo off
 pushd %~dp0
 
-..\..\tools\packager.exe --appdir=..\..\bin\wasm\ --search-path=C:\Users\Schorsch\.nuget\packages\fsharp.core\4.7.0\lib\netstandard2.0 "..\..\bin\%~1\netstandard2.1\Aardvark.WebAssembly.dll" C:\Users\Schorsch\.nuget\packages\fsharp.core\4.7.0\lib\netstandard2.0\FSharp.Core.dll
+SET OUTPUT=%~dp0..\..\bin\%~1\netstandard2.1
 
-xcopy /Y /R index.html ..\..\bin\wasm\
+..\..\tools\packager.exe --appdir=..\..\bin\wasm\ ^
+   --copy=always ^
+   --search-path=%OUTPUT% ^
+   --asset=%~dp0index.html ^
+   %OUTPUT%\FSharp.Core.dll ^
+   %OUTPUT%\Aardvark.WebAssembly.dll ^
+   System.IO.dll ^
+   System.ValueTuple.dll ^
+   System.Reflection.dll ^
+   System.Threading.dll ^
+   System.Reflection.Emit.Lightweight.dll ^
+   System.Reflection.Emit.ILGeneration.dll ^
+   System.Core.dll ^
+   System.Runtime.dll ^
+   System.Console.dll ^
+   System.Globalization.dll ^
+   System.Runtime.Extensions.dll ^
+   System.Dynamic.Runtime.dll ^
+   System.ObjectModel.dll ^
+   System.Buffers.dll ^
+   %OUTPUT%\FSharp.Data.Adaptive.dll
+
+   REM netstandard.dll ^
+   REM System.Runtime.dll ^
+   REM System.dll ^
+
+   REM %OUTPUT%\System.Runtime.CompilerServices.Unsafe.dll ^
+   REM %OUTPUT%\System.Reflection.TypeExtensions.dll ^
+   REM %OUTPUT%\System.Reflection.Metadata.dll ^
+   REM %OUTPUT%\System.ObjectModel.dll ^
+   REM %OUTPUT%\System.Numerics.Vectors.dll ^
+   REM %OUTPUT%\System.Linq.dll ^
+   REM %OUTPUT%\System.Linq.Expressions.dll ^
+   REM %OUTPUT%\System.Dynamic.Runtime.dll ^
+   REM %OUTPUT%\System.Buffers.dll ^
+   REM %OUTPUT%\System.Threading.dll ^
+   REM %OUTPUT%\FSharp.Data.Adaptive.dll ^
 
 popd
